08e9bf061c50784f7bfdcb5b01290d9a73187cbb
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-02-09 16:16:02 +0530

    Added admin interface basics and ability to stop server

--- muzicast/web/admin.py
+++ muzicast/web/admin.py
@@ -41,3 +41,18 @@ def login():
         else:
             wrong = True
     return render_template('admin/login.html', wrong=wrong)
+
+@admin.route('/stop', methods=['POST'])
+def stop():
+    signal.alarm(1)
+    #TODO: do cleanup
+    return render_template('admin/stopped.html')
+    #TODO: do more cleanup
+
+@admin.route('/password', methods=['POST'])
+def change_password():
+    pass
+
+def shutdown(signal, stack_frame):
+    sys.exit(0)
+signal.signal(signal.SIGALRM, shutdown)
