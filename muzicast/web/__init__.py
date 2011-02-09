914e419cd753f6815b2aa308b49d7ed357b523d6
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-02-09 16:16:25 +0530

    Add handler modules as required

--- muzicast/web/__init__.py
+++ muzicast/web/__init__.py
@@ -6,4 +6,12 @@ app = Flask(__name__)
 from muzicast.web.admin import admin
 app.register_module(admin, url_prefix='/admin')
 
+#from muzicast.web.music import artist, album, track
+#app.register_module(artist, url_prefix='/artist')
+#app.register_module(album, url_prefix='/album')
+#app.register_module(track, url_prefix='/track')
+
+from muzicast.web.main import main
+app.register_module(main, url_prefix='/')
+
 app.secret_key = os.urandom(24)
