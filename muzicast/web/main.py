cb59cc23a19f6cc5f1eecb32adca2be4007053c4
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-03-01 23:00:37 +0530

    Added fake top and recently played track functions

--- muzicast/web/main.py
+++ muzicast/web/main.py
@@ -1,8 +1,23 @@
 from flask import Module, render_template, url_for, redirect, session, escape, request
 
 from muzicast.web.util import is_first_run
+from muzicast.meta import Track
 
 main = Module(__name__)
+
+def top_tracks(n):
+    """
+    Returns the top n tracks
+    """
+    #TODO(nikhil) fix this to use statistics
+    return [Track.get(i) for i in range(1, n+1)]
+
+def recently_played(n):
+    """
+    Returns n latest played tracks
+    """
+    #TODO(nikhil) fix this to use statistics
+    return [Track.get(i) for i in range(1, n+1)]
 
 @main.route('/')
 def index():
@@ -11,4 +26,4 @@ def index():
         return redirect(url_for('admin.index'))
 
     # TODO: will need attributes for template
-    return render_template('home.html')
+    return render_template('home.html', top_tracks=top_tracks, recently_played=recently_played)
