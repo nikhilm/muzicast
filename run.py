e82880e166c1722b17dc56958acf674a13a4eb2b
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-03-06 01:01:49 +0530

    Proof of concept process based runner.
    Scanner and streamer are started in a subprocess. This
    requires that use_reloader be set to False in Flask,
    otherwise it will respawn the process and launch the sub
    processes twice.

--- run.py
+++ run.py
@@ -1,2 +1,30 @@
+import os
+import time
+import sys
+import signal
+import subprocess
+
+from muzicast.const import BASEDIR
 from muzicast.web import app
-app.run('0.0.0.0', 7664, debug=True)
+
+print 'Running', os.getpid(), os.getppid()
+
+class Runner(object):
+    def run(self):
+        self.streamer = subprocess.Popen([sys.executable, os.path.join(BASEDIR, 'streamer.py')])
+        self.scanner = subprocess.Popen([sys.executable, os.path.join(BASEDIR, 'collection/__init__.py')])
+        print 'Started streamer PID %d'%self.streamer.pid
+        print 'Started scanner PID %d'%self.scanner.pid
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+        #app.run('0.0.0.0', 7664, debug=True, use_reloader=False)
+        app.run('0.0.0.0', 7664, debug=False, use_reloader=False)
+
+    def shutdown(self, signum, frame):
+        self.streamer.terminate()
+        self.scanner.terminate()
+        sys.exit(0)
+
+if __name__ == '__main__':
+    r = Runner()
+    r.run()
