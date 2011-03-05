a6c64ef212149a2b7fbbeb45a070fa632275b749
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-03-06 01:00:42 +0530

    Include playlist on track and album pages

--- muzicast/web/track.py
+++ muzicast/web/track.py
@@ -2,7 +2,7 @@ from flask import Module, render_template, url_for, redirect, session, escape, r
 
 from muzicast.const import DB_FILE
 from muzicast.meta import Track
-from muzicast.web.playlist import add_to_playlist, delete_from_playlist
+from muzicast.web import playlist
 
 track = Module(__name__)
 
@@ -13,14 +13,14 @@ def tracks():
 @track.route('/<int:id>')
 def index(id):
     track = Track.get(id)
-    return render_template("track.html", track=track)
+    return render_template("track.html", track=track, playlist=playlist)
 
 @track.route('/add/<int:id>')
 def add_track_to_playlist(id):
-    add_to_playlist(id)
+    playlist.add_to_playlist(id)
     return redirect(request.headers['referer'])
 
 @track.route('/delete/<int:id>')
 def delete_track_from_playlist(id):
-    delete_from_playlist(id)
+    playlist.delete_from_playlist(id)
     return redirect(request.headers['referer'])
