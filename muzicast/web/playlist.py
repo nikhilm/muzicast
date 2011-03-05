cf92a07c7ff3fe4943a3dc85fa781ae607379f63
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-03-05 17:39:53 +0530

    Added session playlist manipulation functions

---
+++ muzicast/web/playlist.py
@@ -0,0 +1,31 @@
+from flask import session
+
+from muzicast.meta import Track
+
+def add_to_playlist(tid):
+    if 'playlist' not in session:
+    	session['playlist'] = set()
+    print 'adding', tid, session['playlist']
+    session['playlist'].add(tid)
+    session.modified = True
+
+def delete_from_playlist(tid):
+    if 'playlist' not in session:
+    	return
+    session['playlist'].discard(tid)
+    session.modified = True
+
+def playlist_contains(tid):
+    if 'playlist' not in session:
+    	return False
+    return tid in session['playlist']
+
+def playlist_clear():
+    del session['playlist']
+    session.modified = True
+
+def playlist():
+    if 'playlist' not in session:
+    	return []
+    print session['playlist']
+    return (Track.get(id) for id in session['playlist'])
