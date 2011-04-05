8fef6606f8f3da2a9acf04e12a72e3161b111fe6
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-04-05 15:31:16 +0530

    Genre full implementation

--- muzicast/web/genre.py
+++ muzicast/web/genre.py
@@ -1,17 +1,40 @@
+from sqlobject import DESC
+from sqlobject.main import SQLObjectNotFound
 from flask import Module, url_for, redirect, session, escape, request
 
 from muzicast.const import DB_FILE
-from muzicast.meta import Genre
-from muzicast.web.util import render_master_page
+from muzicast.meta import Genre, GenreStatistics
+from muzicast.web import playlist
+from muzicast.web.util import render_master_page, page_view
 
 genre = Module(__name__)
 
+def top_genres(n):
+    try:
+        return [t.genre for t in GenreStatistics.select(orderBy=DESC(GenreStatistics.q.play_count))[:10]]
+    except SQLObjectNotFound:
+        return []
+
 @genre.route('s')
 def genres():
-    return "TODO: fixme"
+    return genres_page(1)
+
+@genre.route('s/<int:page>')
+def genres_page(page):
+    return page_view(page, Genre, "genres.html", "genres", top_genres=top_genres(10))
 
 @genre.route('/<id>')
 def index(id):
     #TODO(nikhil) handle exception
     genre = Genre.get(id)
-    return render_master_page("genre.html", title="Genre", genre=genre)
+    return render_master_page("genre.html", title="genre", genre=genre)
+
+@genre.route('/add/<int:id>')
+def add_genre_to_playlist(id):
+    try:
+        genre = Genre.get(id)
+        for track in genre.tracks:
+            playlist.add_to_playlist(track.id)
+        return redirect(request.headers['referer'])
+    except LookupError:
+        abort(400)
