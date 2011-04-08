bc7e5aff7773f49ca32eb2e163da85ff6bbb1882
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-04-09 01:37:58 +0530

    Bring interface in line with user manual

--- muzicast/web/track.py
+++ muzicast/web/track.py
@@ -2,7 +2,7 @@ from sqlobject.main import SQLObjectNotFound
 from flask import Module, render_template, url_for, redirect, session, escape, request, current_app, abort
 
 from muzicast.const import DB_FILE, STREAM_PORT
-from muzicast.meta import Track
+from muzicast.meta import Track, TrackStatistics
 from muzicast.web import playlist
 from muzicast.web.util import page_view, render_master_page
 
@@ -20,13 +20,19 @@ def tracks_page(page):
 def index(id):
     try:
         track = Track.get(id)
+        stats = list(TrackStatistics.select(TrackStatistics.q.track == track.id))
+        if len(stats) > 0:
+        	stats = stats[0]
+        else:
+        	stats = None
+
         url = request.environ['HTTP_HOST']
         pos = url.rfind(':' + request.environ['SERVER_PORT'])
         if pos != -1:
             url = url[:pos]
         # otherwise the host is just the host without a port,
         # which is just what we want
-        return render_master_page("track.html", title='Muzicast: Track', track=track, url=url, port=STREAM_PORT)
+        return render_master_page("track.html", title='Muzicast: Track', track=track, url=url, port=STREAM_PORT, stats=stats)
     except SQLObjectNotFound:
         abort(404)
 
