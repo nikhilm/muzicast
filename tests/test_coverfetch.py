34a2afa41c3186e9455fb420c9d45a29c9832d94
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-04-09 23:17:56 +0530

    Coverfetch updates

--- tests/test_coverfetch.py
+++ tests/test_coverfetch.py
@@ -8,10 +8,10 @@ from nose.tools import *
 from muzicast.collection.coverfetch import fetch_cover
 
 def test_success():
-    assert fetchcover('Arcade Fire', 'Neon Bible', '/tmp/test-cover-1.png') == 'test-cover-1.png'
+    assert fetch_cover('Arcade Fire', 'Neon Bible', '/tmp/test-cover-1.png') == '/tmp/test-cover-1.png'
 
 def test_bad_artist():
-    assert fetchcover('abcdefghijklmnopqrstuvwxyz', 'Neon Bible', '/tmp/test-cover-2.png') == None
+    assert fetch_cover('abcdefghijklmnopqrstuvwxyz', 'Neon Bible', '/tmp/test-cover-2.png') == None
 
 def test_bad_album():
-    assert fetchcover('Arcade Fire', 'they have no album like this', '/tmp/test-cover-3.png') == None
+    assert fetch_cover('Arcade Fire', 'they have no album like this', '/tmp/test-cover-3.png') == None
